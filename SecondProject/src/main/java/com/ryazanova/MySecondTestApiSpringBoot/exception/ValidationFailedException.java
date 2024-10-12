3b921ff6f091bf150e7be40bf96c9f744c567781
sanchovy24 <151055560+sanchovy24@users.noreply.github.com>
2024-10-12 14:51:01 +0600

    Delete SecondProject/src/main/java/com/ryazanova/MySecondTestApiSpringBoot directory

--- SecondProject/src/main/java/com/ryazanova/MySecondTestApiSpringBoot/exception/ValidationFailedException.java
+++
@@ -1,5 +0,0 @@
-package com.ryazanova.MySecondTestApiSpringBoot.exception;
-
-public class ValidationFailedException extends Throwable {
-    public ValidationFailedException(String message) { super(message);}
-}
--- SecondProject/src/main/java/com/ryazanova/MySecondTestApiSpringBoot/service/ValidationService.java
+++
@@ -1,9 +0,0 @@
-package com.ryazanova.MySecondTestApiSpringBoot.service;
-import org.springframework.stereotype.Service;
-import org.springframework.validation.BindingResult;
-import com.ryazanova.MySecondTestApiSpringBoot.exception.ValidationFailedException;
-
-@Service
-public interface ValidationService {
-    void isValid(BindingResult bindingResult) throws ValidationFailedException;
-}
